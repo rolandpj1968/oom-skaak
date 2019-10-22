ddcfb00e755a5c61cd19b13ed766389bedb50b45
rolandpj1968 <rolandpj@gmail.com>
2019-10-22 15:11:43 +0200

    Minor clean up

--- src/perft/perft.hpp
+++ src/perft/perft.hpp
@@ -481,46 +481,6 @@ namespace Chess {
       return mySliderPinnedPiecesBb;
     }
     
-    inline BitBoardT genDiagPinnedPiecesBb(const SquareT myKingSq, const BitBoardT allPiecesBb, const BitBoardT allMyPiecesBb, const ColorStateT& yourState) {
-	// Diagonally pinned pieces
-	BitBoardT myKingDiagAttackersBb = bishopAttacks(myKingSq, allPiecesBb);
-	// Potentially pinned pieces are my pieces that are on an open diagonal from my king
-	BitBoardT myCandidateDiagPinnedPiecesBb = myKingDiagAttackersBb & allMyPiecesBb;
-	// Your pinning pieces are those that attack my king once my candidate pinned pieces are removed from the board
-	BitBoardT myKingDiagXrayAttackersBb = bishopAttacks(myKingSq, (allPiecesBb & ~myCandidateDiagPinnedPiecesBb));
-	// We don't want direct attackers of the king, but only attackers that were exposed by removing our candidate pins.
-	BitBoardT yourDiagPinnersBb = myKingDiagXrayAttackersBb & ~myKingDiagAttackersBb & (yourState.bbs[Bishop] | yourState.bbs[Queen]);
-	// Then my pinned pieces are those candidate pinned pieces that lie on one of your pinners' diagonals
-	BitBoardT pinnerDiagonalsBb = BbNone;
-	for(BitBoardT bb = yourDiagPinnersBb; bb;) {
-	  SquareT pinnerSq = Bits::popLsb(bb);
-	  pinnerDiagonalsBb |= bishopAttacks(pinnerSq, allPiecesBb);
-	}
-	BitBoardT myDiagPinnedPiecesBb = myCandidateDiagPinnedPiecesBb & pinnerDiagonalsBb;
-
-	return myDiagPinnedPiecesBb;
-    }
-    
-    inline BitBoardT genOrthogPinnedPiecesBb(const SquareT myKingSq, const BitBoardT allPiecesBb, const BitBoardT allMyPiecesBb, const ColorStateT& yourState) {
-	// Orthogonally pinned pieces
-	BitBoardT myKingOrthogAttackersBb = rookAttacks(myKingSq, allPiecesBb);
-	// Potentially pinned pieces are my pieces that are on an open orthogonal from my king
-	BitBoardT myCandidateOrthogPinnedPiecesBb = myKingOrthogAttackersBb & allMyPiecesBb;
-	// Your pinning pieces are those that attack my king once my candidate pinned pieces are removed from the board
-	BitBoardT myKingOrthogXrayAttackersBb = rookAttacks(myKingSq, (allPiecesBb & ~myCandidateOrthogPinnedPiecesBb));
-	BitBoardT yourOrthogPinnersBb = myKingOrthogXrayAttackersBb & ~myKingOrthogAttackersBb & (yourState.bbs[Rook] | yourState.bbs[Queen]);
-	// Then my pinned pieces are those candidate pinned pieces that lie on one of your pinners' orthogonals
-	BitBoardT pinnerOrthogonalsBb = BbNone;
-	for(BitBoardT bb = yourOrthogPinnersBb; bb;) {
-	  SquareT pinnerSq = Bits::popLsb(bb);
-	  pinnerOrthogonalsBb |= rookAttacks(pinnerSq, allPiecesBb);
-	}
-	// Gack - picks up pieces on the other side of the king
-	BitBoardT myOrthogPinnedPiecesBb = myCandidateOrthogPinnedPiecesBb & pinnerOrthogonalsBb;
-
-	return myOrthogPinnedPiecesBb;
-    }
-    
     template <ColorT Color, typename MyBoardTraitsT, typename YourBoardTraitsT>
     inline void perftImplFull(PerftStatsT& stats, const BoardT& board, const int depthToGo, const MoveInfoT moveInfo) {
       
@@ -571,9 +531,7 @@ namespace Chess {
 	// Calculate pins
       
 	// Find my pinned pieces - used to filter out invalid moves due to discovered check on my king
-	//BitBoardT myDiagPinnedPiecesBb = genDiagPinnedPiecesBb(myKingSq, allPiecesBb, allMyPiecesBb, yourState);
 	BitBoardT myDiagPinnedPiecesBb = genPinnedPiecesBb<Diagonal>(myKingSq, allPiecesBb, allMyPiecesBb, yourState);
-	//BitBoardT myOrthogPinnedPiecesBb = genOrthogPinnedPiecesBb(myKingSq, allPiecesBb, allMyPiecesBb, yourState);
 	BitBoardT myOrthogPinnedPiecesBb = genPinnedPiecesBb<Orthogonal>(myKingSq, allPiecesBb, allMyPiecesBb, yourState);
 
 	// Evaluate moves
