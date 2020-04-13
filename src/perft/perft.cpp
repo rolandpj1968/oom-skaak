b95c5d5dc959f6bcb68dbf49dcd1e5432ea912df
rolandpj1968 <rolandpj@gmail.com>
2020-04-13 16:07:21 +0200

    Perft with FEN - lots of errors from standard positions to debug which is cool

--- src/perft/perft.cpp
+++ src/perft/perft.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 #include "board.hpp"
+#include "fen.hpp"
 #include "perft.hpp"
 
 using namespace Chess;
@@ -9,17 +10,23 @@ using namespace Perft;
 
 // perft <depth>
 int main(int argc, char* argv[]) {
-  if(argc <= 1) {
-    fprintf(stderr, "usage: %s <depth>\n", argv[0]);
+  if(argc <= 1 || argc > 3) {
+    fprintf(stderr, "usage: %s <depth> [FEN]\n", argv[0]);
     exit(1);
   }
   int depthToGo = atoi(argv[1]);
 
   printf("sizeof(BoardT) is %lu\n", sizeof(BoardT));
   
-  BoardT startingBoard = Board::startingPosition();
+  BoardT board;
+  if(argc == 2) {
+    board = Board::startingPosition();
+  } else {
+    // TODO need to get traits and color to move from FEN too
+    board = Fen::parseFen(argv[2]).first;
+  }
 
-  PerftStatsT stats = perft<StartingBoardTraitsT>(startingBoard, depthToGo);
+  PerftStatsT stats = perft<StartingBoardTraitsT>(board, depthToGo);
 
   printf("perft(%d) - nodes = %lu, captures = %lu, eps = %lu, castles = %lu, checks = %lu, discoveries = %lu, doublechecks = %lu, checkmates = %lu\n", depthToGo, stats.nodes, stats.captures, stats.eps, stats.castles, stats.checks, stats.discoverychecks, stats.doublechecks, stats.checkmates);
   printf("           invalids = %lu, invalidsnon0 = %lu, non0-in-pin-path = %lu, diag-pins = %lu, orthog-pins = %lu\n", stats.invalids, stats.invalidsnon0, stats.non0inpinpath, stats.non0withdiagpins, stats.non0withorthogpins);
