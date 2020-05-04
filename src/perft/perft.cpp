222871e2779a80036529ce67b603bb1b4702592e
rolandpj1968 <rolandpj@gmail.com>
2020-05-04 15:58:56 +0200

    Some new cmd-line params and interesting to note that -finline-limit=2000 doubles the perf

--- src/perft/perft.cpp
+++ src/perft/perft.cpp
@@ -8,15 +8,7 @@
 
 using namespace Chess;
 
-// perft <depth>
-int main(int argc, char* argv[]) {
-  if(argc <= 1 || argc > 3) {
-    fprintf(stderr, "usage: %s <depth> [FEN]\n", argv[0]);
-    exit(1);
-  }
-  int depthToGo = atoi(argv[1]);
-
-  if(argc == 3 && std::string(argv[2]) == "RPJ") {
+static void do_special_and_die(int depthToGo) {
     printf("Hallo RPJ\n");
     //auto basicBoard = Fen::parseFen("r3k2r/Pppp1ppp/1b3nbN/nPP5/BB2P3/q4N2/Pp1P2PP/R2Q1RK1 b kq - 0 1").first;
     auto basicBoard = Fen::parseFen("r3k3/Pppp1ppp/1b3nbN/nPP5/BB2P3/q4N2/Pp1P2PP/R2Q1RK1 b q - 0 1").first;
@@ -32,17 +24,58 @@ int main(int argc, char* argv[]) {
     printf("perft(%d) - nodes = %lu, captures = %lu, eps = %lu, castles = %lu, promos = %lu, checks = %lu, discoveries = %lu, doublechecks = %lu, checkmates = %lu\n", depthToGo, stats.nodes, stats.captures, stats.eps, stats.castles, stats.promos, stats.checks, stats.discoverychecks, stats.doublechecks, stats.checkmates);
   
     exit(1);
-  }
+}
 
+static void usage_and_die(int argc, char* argv[], const char* msg = 0) {
+  if(msg) {
+    fprintf(stderr, "%s\n\n", msg);
+  }
+  
+  fprintf(stderr, "usage: %s <depth> [FEN] [--split] [--hash-depth <max-depth>] [--hash-size <size>]\n\n", argv[0]);
+  fprintf(stderr, "  Default position is the starting position; also use \"-\" for starting position, e.g. %s 6 \"-\" --hash 4\n", argv[0]);
+  fprintf(stderr, "  --split provides top-level subtree statistics per top-level move - this is useful for debugging\n");
+  fprintf(stderr, "  --hash <max-depth> enables tableauing of results for repeated positions up to max-depth\n");
+  fprintf(stderr, "      hash tables are only used from level 3 and deeper since no repeat positions are possible nearer the root\n");
+  fprintf(stderr, "  --hash-size <size> defines the maximum hash-table size for each level\n");
+  fprintf(stderr, "      hash table entries are discarded according to LRU\n");
+  fprintf(stderr, "\n");
+  
+  exit(1);
+}
+
+static void dumpStats(const Perft::PerftStatsT& stats) {
+  printf("nodes = %lu, captures = %lu, eps = %lu, castles = %lu, promos = %lu, checks = %lu, discoveries = %lu, doublechecks = %lu, checkmates = %lu\n", stats.nodes, stats.captures, stats.eps, stats.castles, stats.promos, stats.checks, stats.discoverychecks, stats.doublechecks, stats.checkmates);
+  //printf("depth-0-with-eps = %lu, epdiscoveries = %lu, ephorizdiscoveries = %lu, epdiagfromdiscoveries = %lu, epdiagcapturediscoveries = %lu\n", stats.nposwitheps, stats.epdiscoveries, stats.ephorizdiscoveries, stats.epdiagfromdiscoveries, stats.epdiagcapturediscoveries);
+}
+
+int main(int argc, char* argv[]) {
   printf("sizeof(NonPromosColorStateImplT) is %lu - NPieces is %d\n", sizeof(NonPromosColorStateImplT), NPieces);
   printf("sizeof(BasicBoardT) is %lu\n", sizeof(BasicBoardT));
   printf("sizeof(FullBoardT) is %lu\n", sizeof(FullBoardT));
   printf("sizeof(FullColorStateImplT) is %lu\n", sizeof(FullColorStateImplT));
+  printf("\n");
   //printf("sizeof(FullColorStateImpl2T) is %lu\n", sizeof(FullColorStateImpl2T));
   
+  if(argc <= 1) {
+    usage_and_die(argc, argv, "Insufficient cmd-line arguments");
+  }
+  
+  int depthToGo = atoi(argv[1]);
+  if(depthToGo < 0) {
+    usage_and_die(argc, argv, "<depth> must be >= 0");
+  }
+
+  if(argc == 3 && std::string(argv[2]) == "RPJ") {
+    do_special_and_die(depthToGo);
+  }
+
   BasicBoardT board;
   ColorT colorToMove;
-  if(argc == 2) {
+  bool doSplit = false;
+  int maxHashDepth = 0;
+  int hashSize = 4096;
+  
+  if(argc < 3 || std::string(argv[2]) == "-") {
     board = BoardUtils::startingPosition();
     colorToMove = White;
   } else {
@@ -51,13 +84,54 @@ int main(int argc, char* argv[]) {
     colorToMove = boardAndColor.second;
   }
 
+  // Parse flags
+  for(int i = 3; i < argc; i++ ) {
+    std::string arg = argv[i];
+
+    if(arg == "--split") {
+      doSplit = true;
+    } else if(arg == "--hash-depth") {
+      i++;
+      if(argc <= i) {
+	usage_and_die(argc, argv, "--hash-depth missing <max-hash-depth> argument");
+      }
+      maxHashDepth = atoi(argv[i]);
+      if(maxHashDepth < 3 || maxHashDepth > depthToGo || depthToGo < 3) {
+	usage_and_die(argc, argv, "Invalid <max-hash-depth>");
+      }
+    } else if(arg == "--hash-size") {
+      i++;
+      if(argc <= i) {
+	usage_and_die(argc, argv, "--hash-size missing <size> argument");
+      }
+      hashSize = atol(argv[i]);
+      if(hashSize < 1) {
+	usage_and_die(argc, argv, "Invalid hash size");
+      }
+    } else {
+	usage_and_die(argc, argv, "Unrecognised argument");
+    }
+  }
+
   BoardUtils::printBoard<BasicBoardT>(board);
   printf("\n%s\n\n", Fen::toFen<BasicBoardT>(board, colorToMove).c_str());
+  bool doNewline = false;
+  if(doSplit) {
+    printf("  providing split results per move at depth 1\n");
+    doNewline = true;
+  }
+  if(maxHashDepth != 0) {
+    printf("  using hash tables with %d entries at depths 3 - %d\n", hashSize, maxHashDepth);
+    doNewline = true;
+  }
+  if(doNewline) {
+    printf("\n");
+  }
 
   Perft::PerftStatsT stats = colorToMove == White ?
     Perft::perft<BasicBoardT, White>(board, depthToGo) :
     Perft::perft<BasicBoardT, Black>(board, depthToGo);
 
-  printf("perft(%d) - nodes = %lu, captures = %lu, eps = %lu, castles = %lu, promos = %lu, checks = %lu, discoveries = %lu, doublechecks = %lu, checkmates = %lu\n", depthToGo, stats.nodes, stats.captures, stats.eps, stats.castles, stats.promos, stats.checks, stats.discoverychecks, stats.doublechecks, stats.checkmates);
-  printf("depth-0-with-eps = %lu, epdiscoveries = %lu, ephorizdiscoveries = %lu, epdiagfromdiscoveries = %lu, epdiagcapturediscoveries = %lu\n", stats.nposwitheps, stats.epdiscoveries, stats.ephorizdiscoveries, stats.epdiagfromdiscoveries, stats.epdiagcapturediscoveries);
+  printf("perft(%d) stats:\n\n", depthToGo);
+  dumpStats(stats);
 }
