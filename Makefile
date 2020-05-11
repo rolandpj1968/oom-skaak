f9925f1a4d738470d6742bf825ea3c04f65505d7
rolandpj1968 <rolandpj@gmail.com>
2020-05-11 15:49:53 +0200

    Fixed self-deadlock in bounded hash table, doh!

--- Makefile
+++ Makefile
@@ -10,8 +10,8 @@ OBJ_FILES = $(addprefix obj/,$(notdir $(CPP_FILES:.cpp=.o)))
 
 # don't seem to get much better than plain old -O3
 
-#CC_FLAGS = -Wall -std=c++11 -fshort-enums -fno-rtti -finline-limit=2000 -flto -march=native -g -I$(SRC_DIR)
-CC_FLAGS = -Wall -Wuninitialized -std=c++11 -fshort-enums -fno-rtti -finline-limit=2000 -flto -march=native -O3 -I$(SRC_DIR)
+#CC_FLAGS = -Wall -std=c++11 -fshort-enums -fno-rtti -finline-limit=2000 -flto -march=native -g -I$(SRC_DIR) -pthread
+CC_FLAGS = -Wall -Wuninitialized -std=c++11 -fshort-enums -fno-rtti -finline-limit=2000 -flto -march=native -O3 -I$(SRC_DIR) -pthread
 #CC_FLAGS = -Wall -Wuninitialized -std=c++11 -fshort-enums -fno-rtti -flto -march=native -O3 -I$(SRC_DIR)
 #CC_FLAGS = -Wall -std=c++11 -fshort-enums -fno-exceptions -fno-rtti -finline-limit=2000 -flto -march=native -Ofast -I$(SRC_DIR)
 #CC_FLAGS = -fprofile-generate -Wall -std=c++11 -fshort-enums -fno-exceptions -fno-rtti -finline-limit=2000 -flto -march=native -Ofast -I$(SRC_DIR)
@@ -22,6 +22,7 @@ CC_FLAGS = -Wall -Wuninitialized -std=c++11 -fshort-enums -fno-rtti -finline-lim
 #CC_FLAGS = -Wall -std=c++11 -fshort-enums -fno-exceptions -fno-rtti -flto -march=native -O2 -I$(SRC_DIR)
 #CC_FLAGS = -Wall -std=c++11 -I$(SRC_DIR)
 
+#LD_FLAGS = -fshort-enums -fno-rtti -finline-limit=2000 -flto -march=native -g -pthread
 LD_FLAGS = -fshort-enums -fno-rtti -finline-limit=2000 -flto -march=native -O3 -pthread
 #LD_FLAGS = -fshort-enums -fno-exceptions -fno-rtti -finline-limit=2000 -flto -march=native -Ofast
 #LD_FLAGS = -fprofile-generate -fshort-enums -fno-exceptions -fno-rtti -finline-limit=2000 -flto -march=native -Ofast
--- src/bounded-hash-map.hpp
+++ src/bounded-hash-map.hpp
@@ -33,23 +33,20 @@ namespace Chess {
 	max_size_val(max_size),	map(max_size), m(new std::mutex) {}
 
     private:
-      bool remove_lru_entry() {
+      bool remove_lru_entry_locked() {
 	if(mru.empty()) {
 	  return false; // already empty
 	}
 
 	const KeyT& lru_key = mru.back();
-	return remove(lru_key); // removed the lru element - this MUST succeed
+	return remove_locked(lru_key); // removed the lru element - this MUST succeed
       }
 
-      void mru_move_to_front(typename std::list<KeyT>::iterator& mru_it) {
+      void mru_move_to_front_locked(typename std::list<KeyT>::iterator& mru_it) {
 	mru.splice(mru.begin(), mru, mru_it);
       }
 
-    public:
-      bool remove(const KeyT& key) noexcept {
-	std::unique_lock<std::mutex> lock(*m);
-	
+      bool remove_locked(const KeyT& key) noexcept {
 	auto map_it = map.find(key);
 	if(map_it == map.end()) {
 	  return false; // not present
@@ -59,6 +56,13 @@ namespace Chess {
 	map.erase(map_it);
 
 	return true; // was present
+      }
+      
+    public:
+      bool remove(const KeyT& key) noexcept {
+	std::unique_lock<std::mutex> lock(*m);
+
+	return remove_locked(key);
       }
     
       bool copy_if_present(const KeyT& key, ValT& to) noexcept {
@@ -70,7 +74,7 @@ namespace Chess {
 	}
 	
 	// move-to-front of mru
-	mru_move_to_front(map_it->second.mru_it);
+	mru_move_to_front_locked(map_it->second.mru_it);
 	  
 	// Key already present - copy the val
 	to = map_it->second.val;
@@ -84,7 +88,7 @@ namespace Chess {
 	auto map_it = map.find(key);
 	if(map_it != map.end()) {
 	  // move-to-front of mru
-	  mru_move_to_front(map_it->second.mru_it);
+	  mru_move_to_front_locked(map_it->second.mru_it);
 	  
 	  // Key already present - just update the val
 	  map_it->second.val = val;
@@ -94,7 +98,7 @@ namespace Chess {
 
 	// Make space for the new entry
 	if(max_size_val <= map.size()) {
-	  remove_lru_entry();
+	  remove_lru_entry_locked();
 	}
 
 	// Add the new entry
